cebafd78586dc3832e04e954723a3b8a43ee7a1f
ddankhazi <ddankhazi@gmail.com>
2026-05-03 20:19:36 +0200

    Update __init__.py

--- ddContentBrowser/__init__.py
+++ ddContentBrowser/__init__.py
@@ -8,7 +8,7 @@ License: MIT
 
 # Version - SINGLE SOURCE OF TRUTH
 # MUST be defined BEFORE importing other modules that use it!
-__version__ = '1.9.2'
+__version__ = '1.9.5'
 
 # Import all modules
 from .config import ContentBrowserConfig
